af907331983e66224240088c4ac2a73311d380b1
Esteban Vega <stron@me.com>
2019-06-15 19:25:10 +0200

    New stats

--- Bot.py
+++ Bot.py
@@ -193,6 +193,7 @@ class Bot(ChromeDriver):
     def explore(self):
         try:
             now = datetime.now()
+            self.wait(60 * 3)
             self.start_session(cookie_file=self.file_names["cookies"])
             self.get_followers_for(self.username, now=now)
             number_of_new_followers = self.follow_new_followers(now)
